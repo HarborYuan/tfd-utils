70739f49fa44206160a4da6fd426cb6e4d07ae73
Haobo Yuan <haoboyuan@ucmerced.edu>
2025-07-08 23:25:32 -0700

    add tests optimize package

--- src/tfd_utils/pb2/__init__.py
+++ src/tfd_utils/pb2/__init__.py
@@ -1,4 +1,15 @@
-from .example_pb2 import Example, SequenceExample # type: ignore
-from .feature_pb2 import BytesList, Feature, Features, FeatureList, FeatureLists,  Int64List,  FloatList # type: ignore
+# Compile command:
+# protoc --python_out=src --pyi_out=src --proto_path=src src/tfd_utils/pb2/*.proto
 
-__all__ = ["Example", "BytesList", "Int64List", "Feature", "Features", "FeatureList", "FeatureLists", "FloatList"]
+from .example_pb2 import Example, SequenceExample
+from .feature_pb2 import (
+    BytesList,
+    Int64List,
+    FloatList,
+    Feature,
+    Features,
+    FeatureList,
+    FeatureLists,
+)
+
+__all__ = ["Example", "SequenceExample", "BytesList", "Int64List", "FloatList", "Feature", "Features", "FeatureList", "FeatureLists"]
